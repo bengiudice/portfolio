eb9335780171e49cfb29d3607b3702e3e10e211c
benjamin giudice <ben.giudice@gmail.com>
2023-01-21 21:11:06 +0800

    Implement projects page

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -2,13 +2,26 @@
     <title>Contact</title>
 </svelte:head>
 
-<div class="container">
-    <p></p>
-</div>
-
-<style>
-    .container {
-        max-width: 800px;
-        margin: 50px auto;
+<script context="module">
+    export const load = async ({ fetch }) => {
+        const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+        const blogposts = await res.json();
+        return {
+            props: {
+                blogposts,
+            }
+        }
     }
-</style>
+</script>
+<script>
+    export let blogposts;
+</script>
+
+<div class="container">
+    <h1>My projects</h1>
+    <div class="projects">
+        {#each projects as proj}
+        <div class="proj"></div>
+        <h2>{proj.title.substring(0, 20)}</h2>
+    </div>
+</div>
